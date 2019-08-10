ff9833353318892c7f7e3dfb368ba73a7f169f24
Jason Brown <browniefed@gmail.com>
2019-08-10 09:15:45 -0700

    Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const nextProgress = progress + 0.05;
+      if (nextProgress > 1) {
+        return;
+      }
+
+      setProgress(nextProgress);
+    }, 1000);
+  });
+
+  const position = Math.min(progress, 1);
+  const complete = position === 1;
+  const notMoved = position === 0;
+
+  const DIAMETER = 50;
+  const STROKE_WIDTH = 6;
+  const RADIUS = DIAMETER / 2 - STROKE_WIDTH / 2;
+  const CIRCUMFERENCE = Math.PI * RADIUS * 2;
+
+  return (
+    <div className="App">
+      <div
+        className="wrapper"
+        style={{
+          backgroundColor: complete ? "tomato" : "#FFF",
+        }}
+      >
+        <svg viewBox="0 0 50 50" width="600px" height="600px" className="circle-progress">
+          <circle
+            className="progress"
+            cx={DIAMETER / 2}
+            cy={DIAMETER / 2}
+            r={RADIUS}
+            stroke={notMoved ? "transparent" : "tomato"}
+            fill="transparent"
+            strokeWidth={STROKE_WIDTH}
+            style={{
+              strokeDasharray: CIRCUMFERENCE,
+              strokeDashoffset: CIRCUMFERENCE * (1 - position),
+            }}
+          />
+        </svg>
+      </div>
+    </div>
+  );
 }
 
 export default App;
